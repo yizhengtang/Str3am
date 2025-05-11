5fdb3292d32fd7c1e0f0bec6ce8efa783ddbbad7
Yi Zheng Tang <yzhengtang4@gmail.com>
2025-05-11 23:22:23 +0100

    Modified some files for setup

--- frontend/config-overrides.js
+++ frontend/config-overrides.js
@@ -1,28 +1,34 @@
-const webpack = require('webpack');
+const webpack = require("webpack");
 
 module.exports = function override(config) {
   const fallback = config.resolve.fallback || {};
   Object.assign(fallback, {
-    "crypto": require.resolve("crypto-browserify"),
-    "stream": require.resolve("stream-browserify"),
-    "assert": require.resolve("assert"),
-    "http": require.resolve("stream-http"),
-    "https": require.resolve("https-browserify"),
-    "os": require.resolve("os-browserify"),
-    "url": require.resolve("url"),
-    "buffer": require.resolve("buffer"),
-    "process": require.resolve("process/browser")
+    crypto: require.resolve("crypto-browserify"),
+    stream: require.resolve("stream-browserify"),
+    assert: require.resolve("assert"),
+    http: require.resolve("stream-http"),
+    https: require.resolve("https-browserify"),
+    os: require.resolve("os-browserify"),
+    url: require.resolve("url"),
+    buffer: require.resolve("buffer"),
+    process: require.resolve("process/browser"),
+    vm: require.resolve("vm-browserify") // for asn1.js warning
   });
   config.resolve.fallback = fallback;
-  
+
+  config.resolve.alias = {
+    ...config.resolve.alias,
+    "process/browser": require.resolve("process/browser"), // CRITICAL LINE
+  };
+
   config.plugins = (config.plugins || []).concat([
     new webpack.ProvidePlugin({
-      process: 'process/browser',
-      Buffer: ['buffer', 'Buffer']
+      process: "process/browser",
+      Buffer: ["buffer", "Buffer"]
     })
   ]);
-  
-  config.ignoreWarnings = [/Failed to parse source map/]; // Optional: A SPU / WARN that often shows up with polyfills
+
+  config.ignoreWarnings = [/Failed to parse source map/];
 
   return config;
-} 
+};
